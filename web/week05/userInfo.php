534507763c568b34419093d8c0161a95a8df2047
Spencer <wil11054@byui.edu>
2017-10-13 16:40:41 -0600

    FIEXED MY ERROR!!!!!

--- web/week05/userInfo.php
+++ web/week05/userInfo.php
@@ -1,22 +1,12 @@
-<html>
-   <head>
-      <title>Welcome</title>
-	        <?php
-	     $dbUrl = getenv('DATABASE_URL');
+<?php
+   $dbUrl = getenv('DATABASE_URL');
 		 
-		 $dbopts = parse_url($dbUrl);
+   $dbopts = parse_url($dbUrl);
 		 
-		 $dbHost = $dbopts["host"];
-		 $dbPort = $dbopts["port"];
-		 $dbUser = $dbopts["user"];
-		 dbPassword = $dbopts["pass"];
-		 dbName = ltrim($dbopts["path"], '/');
-		 
-		 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-      ?>  
-   </head>
-   
-   <body>
-   <p>HELP!!!</p>
-   </body>
-</html>
+   $dbHost = $dbopts["host"];
+   $dbPort = $dbopts["port"];
+   $dbUser = $dbopts["user"];
+   $dbPassword = $dbopts["pass"];
+   $dbName = ltrim($dbopts["path"], '/');
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+?>
