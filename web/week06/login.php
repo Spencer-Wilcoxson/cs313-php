ee0a956bffd7c1d617a9f06fef0a7811e093953f
Spencer <wil11054@byui.edu>
2017-10-18 18:34:19 -0600

    allow the user to add an event"

--- web/week06/login.php
+++ web/week06/login.php
@@ -13,8 +13,8 @@
 	  <h1 id="welcome">Welcome <?php getUser(); ?></h1>
 	  <p> Please select what you would like to do below</p>
 	  <div id="options">
-	     <input type="button" value="Add Event">
-		 <input type="button" value="Delete Event">
+<!--	     <input type="button" value="Add Event">
+		 <input type="button" value="Delete Event"> -->
 	  </div>
    </body>
 </html>
