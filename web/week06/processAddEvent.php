91c0fd728c02d60430784d5e8a6a58cf5b8a2a02
Spencer <wil11054@byui.edu>
2017-10-20 16:54:45 -0600

    I hope this works...

--- web/week06/processAddEvent.php
+++ web/week06/processAddEvent.php
@@ -1,13 +1,12 @@
 <?php
    session_start();
-   
-   /* redirect to the login screen */
-   header("Location: login.php");
-   
+
    require "dbconnect.php";
    
    /* process the form data*/
    addEvent();
    
+   /* redirect to the login screen */
+   header("Location: login.php");
    exit;
 ?>
