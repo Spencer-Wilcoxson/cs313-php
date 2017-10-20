3c2100783c2361b5607bc7fcc8077c581e8f51b4
Spencer <wil11054@byui.edu>
2017-10-20 16:39:25 -0600

    I hope this works...

--- web/week06/addEvent.php
+++ web/week06/addEvent.php
@@ -11,19 +11,11 @@
       <h1>Add Event</h1>
 	  <p>Please enter the event information below</p>
 	  
-	  <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+	  <form method="post" action="processAddEvent.php">
 	     <input type="text" placeholder="Event Name" name="eventName"> <br />
 		 Event Date: <input type="date" name="eventDate"> <br />
 		 Reminder Date: <input type="date" name="ReminderDate"> <br />
 		 <input type="submit" value="submit">
 	  </form>
-	  
-	  
-	  <!-- Only run this if the form above has been submitted -->
-	  <?php
-         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			 echo "YESSSS!!!!!";
-		 }	  
-	  ?>
    </body>
 </html>
