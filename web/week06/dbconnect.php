30f92fca7267f795041ae93f7cb94fd4f9e8aaa6
Spencer <wil11054@byui.edu>
2017-10-21 20:22:28 -0600

    enabled the user to see the guestlist

--- web/week06/dbconnect.php
+++ web/week06/dbconnect.php
@@ -112,12 +112,12 @@
 	  echo '</tr>';
 //	  echo '</table>';
 	  
-	  $query = "SELECT guestlist.lastname, guestlist.firstname, events.name AS EventName FROM ((junction INNER JOIN guestlist ON junction.guestid = guestlist.id) INNER JOIN events ON junction.eventsid = events.id)";
+	  $query = "SELECT guestlist.lastname, guestlist.firstname, events.name FROM ((junction INNER JOIN guestlist ON junction.guestid = guestlist.id) INNER JOIN events ON junction.eventsid = events.id)";
 	  foreach ($db->query($query) as $row) {
 		  echo '<tr>';
 		  echo '<td>'.$row['lastname'].'</td>';
 		  echo '<td>'.$row['firstname'].'</td>';
-		  echo '<td>'.$row['EventName'].'</td>';
+		  echo '<td>'.$row['name'].'</td>';
 		  echo '</tr>';
 	  }
 	  echo '</table>';
