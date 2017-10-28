d25c081143ac86826da0d95e22861a747d7ffbfa
Spencer <wil11054@byui.edu>
2017-10-28 11:49:12 -0600

    experimenting with AJAX

--- web/week07/inviteList.php
+++ web/week07/inviteList.php
@@ -4,7 +4,26 @@
    require "dbconnect.php";
    connect();
    
+   public class guest {
+	   $lastname = null;
+	   $firstname = null;
+   }
+   
+   
    $db = $GLOBALS['db'];
    $userID = $_SESSION['userID'];
+   
+   $a = array();
+   
+   $query = "SELECT lastname, firstname FROM guestList WHERE userid = $userID";
+   
+   foreach (($db->query($query) as $row) {
+	   $obj = new guest();
+	   $obj->lastname = $row['lastname'];
+	   $obj->firstname = $row['firstname'];
+	   
+	   array_push($a, $obj);
+   }
+   
    echo "HELLO";
 ?>
