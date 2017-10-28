7d5e797ff39b21933942c9c11c1bec78bef0efc6
Spencer <wil11054@byui.edu>
2017-10-27 22:47:31 -0600

    experimenting with AJAX

--- web/week07/addGuest.js
+++ web/week07/addGuest.js
@@ -1,13 +1,4 @@
 function showGuest() {
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			var myObjArray = JSON.parse(this.responseText);
-			document.getElementById("addGuestList").innerHTML = "HELLO???";
-		}
-	};
-	
-	xhttp.open("POST", "getListGuest.php", true);
-	xhttp.send();
-	
+	document.getElementById("addGuestList").innerHTML = "Hello?";
 }
