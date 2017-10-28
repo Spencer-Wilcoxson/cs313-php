7b100493e715dabe9e685e6a5c93190875873e8e
Spencer <wil11054@byui.edu>
2017-10-28 15:01:22 -0600

    experimenting with AJAX

--- web/week07/inviteList.php
+++ web/week07/inviteList.php
@@ -8,7 +8,7 @@
    $userID = $_SESSION['userID'];
    
    
-   $query = "SELECT lastname, firstname FROM guestList WHERE userid = $userID";
+//   $query = "SELECT lastname, firstname FROM guestList WHERE userid = $userID";
    
 //   echo '<table> <tr> <th>Last Name</th> <th>First Name</th> <th>Invite</th></tr></table>';
    echo '<table>';
@@ -18,6 +18,14 @@
    echo '<th>Invite</th>';
    echo '</tr>';
    echo '</table>';
-   foreach (($db->query($query) as $row) {
+   
+   $query = "SELECT lastname, firstname FROM guestlist WHERE userid = $userID";
+   foreach ($db->query($query) as $row) {
+      echo '<tr>';
+	  echo '<td>'.$row['lastname'].'</td>';
+	  echo '<td>'.$row['firstname'].'</td>';
+	  echo '<td><input type="checkbox" class="inviteCheck"></td>';
+	  echo '</tr>';
    }
+   echo '</table>';
 ?>
