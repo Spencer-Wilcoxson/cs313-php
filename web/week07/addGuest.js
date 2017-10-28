ac5e7fdfab320d613cb4c60c992ab894ea3e34bf
Spencer <wil11054@byui.edu>
2017-10-27 22:39:57 -0600

    Allowed the javascript to update the guestlist using ajax

--- web/week07/addGuest.js
+++ web/week07/addGuest.js
@@ -3,33 +3,7 @@ function showGuest() {
 	xhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var myObjArray = JSON.parse(this.responseText);
-			var div = document.getElementById("addGuestList");
-			
-			// create the table
-			var table = document.createElement("table");
-			var th = document.createElement("TH");
-			var lastNameNode = document.createTextNode("Last Name");
-			var firstNameNode = document.createTextNode("First Name");
-			var addBoxNode = document.createTextNode("Add Guest");
-			
-			div.appendChild(firstNameNode);
-			
-			// add the table headers
-//			th.appendChild(lastNameNode);
-//			th.appendChild(firstNameNode);
-//			th.appendChild(addBoxNode);
-			
-			// add the headers to the table
-//			table.appendChild(th);
-			
-//			div.appendChild(table);
-			
-			
-			
-//			for (var i = ; i < myObjArray.length; i++) {
-//				var object = myObjArray[i];
-//				div.innerHTML +=
-//			}
+			window.alert("HELLO??");
 		}
 	};
 	
