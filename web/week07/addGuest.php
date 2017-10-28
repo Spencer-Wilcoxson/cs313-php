801da8c8060ebf23721459cfff5c59cd44d24180
Spencer <wil11054@byui.edu>
2017-10-28 16:26:26 -0600

    experimenting with AJAX

--- web/week07/addGuest.php
+++ web/week07/addGuest.php
@@ -12,14 +12,14 @@
    
    $obj = json_decode($_POST['guests'], false);
    
-   for ($i = 0; $i < $obj.count(); $i++) {
-	   $getGuestQuery = "SELECT * FROM guestlist WHERE lastname = '$obj[i]->lastname' AND firstname = '$obj[i]->firstname'";
+//   for ($i = 0; $i < $obj.count(); $i++) {
+//	   $getGuestQuery = "SELECT * FROM guestlist WHERE lastname = '$obj[i]->lastname' AND firstname = '$obj[i]->firstname'";
 	   
 	   
-	   foreach(($db->query($getGuestQuery)) as $row) {
+//	   foreach(($db->query($getGuestQuery)) as $row) {
 	   
-	      $guestId = $row['id'];
-	   }
+//	      $guestId = $row['id'];
+//	   }
 	   
 	   $query = "INSERT INTO junction (eventsid, guestid, userid) VALUES (5, 9, $userID)";
 	   
